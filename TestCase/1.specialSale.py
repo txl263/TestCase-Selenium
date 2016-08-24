af30010b8a73ed8ad75b9c666226b1a9b0ae569c
txl263 <xiaolei.tien@gmail.com>
2016-08-24 17:30:33 +0800

    2016-8-24 17:30:29

--- TestCase/1.specialSale.py
+++ TestCase/1.specialSale.py
@@ -12,7 +12,7 @@ import unittest,time,re
 #setUp 用于设置初始化的部分，在测试用例执行前，这个方法中的函数将先被调用。这里将浏览器的调用和URL的访问放到初始化部分。#
 class Wukong(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Safari()
+        self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
         self.baseUrl = "http://test3.wukonglicai.com"
         self.verificationErrors=[]  #脚本运行时，错误的信息将被打印到这个列表中#
@@ -20,7 +20,7 @@ class Wukong(unittest.TestCase):
     def test_specialSale_obj_pro(self):
         driver = self.driver
         driver.get(self.baseUrl + "/weixin/specialSale/index.html")
-        driver.find_element_by_xpath("html/body/div[1]/div[1]/a/div[1]/p/span[1]").send_keys("13011898794")
+        driver.find_element_by_xpath("html/body/div[1]/div[1]/a/div[1]/p/span[1]")
         time.sleep(2)
 
     def is_element_present(self, how, what):
