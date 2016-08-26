816742d2abfc5dc43d06a65d4880e348a60654f4
txl263 <eric@172-0-12-11.lightspeed.wlfrct.sbcglobal.net>
2016-08-26 15:18:49 +0800

    修改冲突

--- TestCase/1.specialSale.py
+++ TestCase/1.specialSale.py
@@ -12,7 +12,7 @@ import unittest,time,re
 #setUp 用于设置初始化的部分，在测试用例执行前，这个方法中的函数将先被调用。这里将浏览器的调用和URL的访问放到初始化部分。#
 class Wukong(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Safari()
         self.driver.implicitly_wait(30)
         self.baseUrl = "http://test3.wukonglicai.com"
         self.verificationErrors=[]  #脚本运行时，错误的信息将被打印到这个列表中#
@@ -22,7 +22,6 @@ class Wukong(unittest.TestCase):
         driver.get(self.baseUrl + "/weixin/specialSale/index.html")
         driver.find_element_by_id("bt").click()
         print (driver.find_element_by_xpath("html/body/div[1]/div[1]/a/div[1]/p/span[1]").text)
-        
         time.sleep(2)
 
     def is_element_present(self, how, what):
