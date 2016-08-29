0ad1647bc30afda7bd0490178dc0aa12f96a391a
txl263 <xiaolei.tien@gmail.com>
2016-08-29 18:02:39 +0800

    2016-8-29 18:02:36

--- TestCase/1.specialSale.py
+++ TestCase/1.specialSale.py
@@ -9,6 +9,13 @@ from selenium.common import exceptions
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest,time,re
+import xml.etree.ElementTree as ET
+tree = ET.parse('specialSale_1.xml')
+root = tree.getroot()
+#for child_of_root in root:
+    #print child_of_root.tag, child_of_root.attrib
+
+
 #setUp 用于设置初始化的部分，在测试用例执行前，这个方法中的函数将先被调用。这里将浏览器的调用和URL的访问放到初始化部分。#
 class Wukong(unittest.TestCase):
     def setUp(self):
@@ -17,11 +24,16 @@ class Wukong(unittest.TestCase):
         self.baseUrl = "http://m.wukonglicai.com"
         self.verificationErrors=[]  #脚本运行时，错误的信息将被打印到这个列表中#
         self.accept_next_alert=True  #是否继续接受下一个警告#
+
     def test_specialSale_obj_pro(self):
         driver = self.driver
         driver.get(self.baseUrl + "/weixin/specialSale/index.html")
         driver.find_element_by_id("bt").click()
-        print (driver.find_element_by_xpath("html/body/div[1]/div[1]/a/div[1]/p/span[1]").text)
+        for i in range(0,len(root)):
+        	if root[i][0].text == "XPath":
+	            if driver.find_element_by_xpath(root[i][1].text) == root[i][2].text :
+
+					print "Success!"
         time.sleep(2)
 
     def is_element_present(self, how, what):
